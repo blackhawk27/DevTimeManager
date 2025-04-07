55bbf9055622e2db3537dc6cecf279e12d65c8f1
jon <endlessfrog@gmail.com>
2025-04-07 15:29:01 +0200

    Step Definitioner oprettet så de virker

--- src/test/java/hellocucumber/CreateProjectSteps.java
+++ src/test/java/hellocucumber/CreateProjectSteps.java
@@ -1,37 +1,57 @@
 package hellocucumber;
 
+import app.ProjectSystem;
 import io.cucumber.java.en.*;
+import app.Employee;
+import app.Project;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CreateProjectSteps {
-    @Given("an employee is logged in")
-    public void anEmployeeIsLoggedIn() {
+    private Employee employee;
+    private ProjectSystem projectSystem = new ProjectSystem();
+    private Project createdProject;
+
+    @Given("an employee with name {string} is logged in")
+    public void anEmployeeWithNameIsLoggedIn(String arg0) {
+        employee = new Employee(arg0);
+        employee.logIn();
+        assertTrue(employee.isLoggedInStatus());
     }
 
-    @And("there exists no projects in the system")
-    public void thereExistsNoProjectsInTheSystem() {
+    @When("the employee creates a project")
+    public void theEmployeeCreatesAProject() {
     }
 
-    @When("the employee chooses {string}")
-    public void theEmployeeChooses(String arg0) {
+    @When("the employee inputs name {string}")
+    public void theEmployeeInputsName(String name) {
+        employee.inputProjectName(name);
     }
 
-    @And("the employee inputs name {string}")
-    public void theEmployeeInputsName(String arg0) {
+    @When("the employee inputs start date {string}")
+    public void theEmployeeInputsStartDate(String dateStr) {
+        LocalDate date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        employee.inputStartDate(date);
     }
 
-    @And("the employee inputs start date {string}")
-    public void theEmployeeInputsStartDate(String arg0) {
-    }
-
-    @And("the employee inputs end date {string}")
-    public void theEmployeeInputsEndDate(String arg0) {
+    @When("the employee inputs end date {string}")
+    public void theEmployeeInputsEndDate(String dateStr) {
+        LocalDate date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        employee.inputEndDate(date);
     }
 
     @Then("the system creates the project {string}")
-    public void theSystemCreatesTheProject(String arg0) {
+    public void theSystemCreatesTheProject(String expectedName) {
+        createdProject = employee.createProject(projectSystem);
+        assertEquals(expectedName, createdProject.getName());
     }
 
-    @And("the system returns a project ID {string}")
-    public void theSystemReturnsAProjectID(String arg0) {
+    @Then("the system returns a project ID {string}")
+    public void theSystemReturnsAProjectID(String expectedId) {
+        assertEquals(expectedId, createdProject.getId());
     }
+
 }
