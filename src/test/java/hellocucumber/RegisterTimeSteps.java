70fd6f98cd8a8f9fb2b80ed099e940cbb0921a21
jon <endlessfrog@gmail.com>
2025-04-07 16:04:11 +0200

    Har tilføjet PendingException()

--- src/test/java/hellocucumber/RegisterTimeSteps.java
+++ src/test/java/hellocucumber/RegisterTimeSteps.java
@@ -1,5 +1,6 @@
 package hellocucumber;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,29 +9,36 @@ import io.cucumber.java.en.When;
 public class RegisterTimeSteps {
     @Given("the employee is logged in")
     public void theEmployeeIsLoggedIn() {
+        throw new PendingException();
     }
 
     @And("that an employee is assigned to an {string} under {string}")
     public void thatAnEmployeeIsAssignedToAnUnder(String arg0, String arg1) {
+        throw new PendingException();
     }
 
     @When("the employee registers in the system that he arrived at {int}:{int} and left at {int}:{int}")
     public void theEmployeeRegistersInTheSystemThatHeArrivedAtAndLeftAt(int arg0, int arg1, int arg2, int arg3) {
+        throw new PendingException();
     }
 
     @Then("the employee has worked {int} hours on {string}")
     public void theEmployeeHasWorkedHoursOn(int arg0, String arg1) {
+        throw new PendingException();
     }
 
     @And("he has registered his time from {int}:{int} to {int}:{int}")
     public void heHasRegisteredHisTimeFromTo(int arg0, int arg1, int arg2, int arg3) {
+        throw new PendingException();
     }
 
     @When("the employee later corrects time spent in the system to say he arrived at {int}:{int} and left at {int}:{int}")
     public void theEmployeeLaterCorrectsTimeSpentInTheSystemToSayHeArrivedAtAndLeftAt(int arg0, int arg1, int arg2, int arg3) {
+        throw new PendingException();
     }
 
     @Then("the employee has worked {int} hours on {string} instead of {int}")
     public void theEmployeeHasWorkedHoursOnInsteadOf(int arg0, String arg1, int arg2) {
+        throw new PendingException();
     }
 }
