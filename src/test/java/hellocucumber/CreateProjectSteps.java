f4364feefc74a43ef68784b64fa0db6b3f09f2ad
jon <endlessfrog@gmail.com>
2025-04-07 14:19:53 +0200

    Har fjernet StepDefinitions og lavet den om til CreateProjectSteps

---
+++ src/test/java/hellocucumber/CreateProjectSteps.java
@@ -0,0 +1,37 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+public class CreateProjectSteps {
+    @Given("an employee is logged in")
+    public void anEmployeeIsLoggedIn() {
+    }
+
+    @And("there exists no projects in the system")
+    public void thereExistsNoProjectsInTheSystem() {
+    }
+
+    @When("the employee chooses {string}")
+    public void theEmployeeChooses(String arg0) {
+    }
+
+    @And("the employee inputs name {string}")
+    public void theEmployeeInputsName(String arg0) {
+    }
+
+    @And("the employee inputs start date {string}")
+    public void theEmployeeInputsStartDate(String arg0) {
+    }
+
+    @And("the employee inputs end date {string}")
+    public void theEmployeeInputsEndDate(String arg0) {
+    }
+
+    @Then("the system creates the project {string}")
+    public void theSystemCreatesTheProject(String arg0) {
+    }
+
+    @And("the system returns a project ID {string}")
+    public void theSystemReturnsAProjectID(String arg0) {
+    }
+}
