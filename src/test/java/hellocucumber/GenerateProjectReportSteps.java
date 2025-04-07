e44a873f3176cc206bb5273e1224e769cee0af13
jon <endlessfrog@gmail.com>
2025-04-07 16:03:29 +0200

    Har tilføjet PendingException()

--- src/test/java/hellocucumber/GenerateProjectReportSteps.java
+++ src/test/java/hellocucumber/GenerateProjectReportSteps.java
@@ -1,5 +1,6 @@
 package hellocucumber;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,45 +8,56 @@ import io.cucumber.java.en.When;
 public class GenerateProjectReportSteps {
     @And("{string} has a budgeted time of {int} hours")
     public void hasABudgetedTimeOfHours(String arg0, int arg1) {
+        throw new PendingException();
     }
 
     @And("the total registered work time on {string} is {int} hours")
     public void theTotalRegisteredWorkTimeOnIsHours(String arg0, int arg1) {
+        throw new PendingException();
     }
 
     @And("the project manager is logged in")
     public void theProjectManagerIsLoggedIn() {
+        throw new PendingException();
     }
 
     @When("the project manager generates a report for {string}")
     public void theProjectManagerGeneratesAReportFor(String arg0) {
+        throw new PendingException();
     }
 
     @Then("the system displays the total registered hours as {int}")
     public void theSystemDisplaysTheTotalRegisteredHoursAs(int arg0) {
+        throw new PendingException();
     }
 
     @And("the system displays the budgeted time as {int}")
     public void theSystemDisplaysTheBudgetedTimeAs(int arg0) {
+        throw new PendingException();
     }
 
     @And("the system displays the unallocated hours as {int}")
     public void theSystemDisplaysTheUnallocatedHoursAs(int arg0) {
+        throw new PendingException();
     }
 
     @And("the system confirms that the project is within budget")
     public void theSystemConfirmsThatTheProjectIsWithinBudget() {
+        throw new PendingException();
     }
 
     @And("the system displays the estimated remaining work time")
     public void theSystemDisplaysTheEstimatedRemainingWorkTime() {
+        throw new PendingException();
     }
 
     @And("no hours have been registered on {string}")
     public void noHoursHaveBeenRegisteredOn(String arg0) {
+        throw new PendingException();
     }
 
     @And("the system warns {string}")
     public void theSystemWarns(String arg0) {
+        throw new PendingException();
     }
 }
