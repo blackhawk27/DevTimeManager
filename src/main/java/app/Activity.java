8b469af4e75ce8728b79cb3f5ff36547a69ee391
jon <endlessfrog@gmail.com>
2025-04-28 13:42:38 +0200

    Activity class added

--- src/main/java/app/Activity.java
+++ src/main/java/app/Activity.java
@@ -1,4 +1,25 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Activity {
+    private final String name;
+    private final List<Employee> assignedEmployees = new ArrayList<>();
+
+    public Activity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void assignEmployee(Employee employee) {
+        assignedEmployees.add(employee);
+    }
+
+    public boolean isEmployeeAssigned(Employee employee) {
+        return assignedEmployees.contains(employee);
+    }
 }
